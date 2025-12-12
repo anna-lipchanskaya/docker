022de887920fa13325075ff5681b7ad6d246b44f
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2025-12-12 22:23:17 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,20 +2,15 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Используем host.containers.internal для Podman + --network host
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://kubsu:kubsu@host.containers.internal:5432/kubsu"
+    "postgresql+psycopg://kubsu:kubsu@localhost:5432/kubsu"
 )
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-)
 
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
 
